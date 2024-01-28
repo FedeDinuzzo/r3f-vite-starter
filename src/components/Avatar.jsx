6f399e80e6475fcaeca6ec255e4c4e335a48afd6
Fede Di Nuzzo <federicodinuzzo98@gmail.com>
2024-01-28 19:50:03 -0300

    animation finish

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,20 +1,54 @@
+import * as THREE from 'three'
 import React, { useRef, useEffect } from 'react'
 import { useGLTF, useFBX } from "@react-three/drei";
 import { useAnimations } from "@react-three/drei";
+import { useFrame } from '@react-three/fiber';
+import { useControls } from 'leva';
 
 export default function Avatar(props) {
+  const { animation } = props
+  const { headFollow, cursorFollow, wireframe } = useControls({
+    headFollow: false,
+    cursorFollow: false,
+    wireframe: false,
+  })
   const group = useRef();
   const { nodes, materials } = useGLTF("models/attemp.glb");
 
   const { animations: typingAnimation } = useFBX("animations/Typing.fbx");
+  const { animations: standingAnimation } = useFBX("animations/Standing.fbx");
+  const { animations: fallingAnimation } = useFBX("animations/Falling.fbx");
+
 
   typingAnimation[0].name = "Typing";
+  standingAnimation[0].name = "Standing";
+  fallingAnimation[0].name = "Falling";
 
-  const { actions } = useAnimations(typingAnimation, group);
+  const { actions } = useAnimations([typingAnimation[0], standingAnimation[0], fallingAnimation[0]], group);
+
+  useFrame((state) => {
+    if (headFollow) {
+      group.current.getObjectByName("Head").lookAt(state.camera.position)
+    }
+    if (cursorFollow) {
+      const target = new THREE.Vector3(state.mouse.x, state.mouse.y, 1)
+      group.current.getObjectByName("Spine2").lookAt(target)
+    }
+  })
 
   useEffect(() => {
-    actions["Typing"].reset().play()
-  }, []);
+    actions[animation].reset().fadeIn(0.5).play()
+
+    return () => {
+      actions[animation].reset().fadeOut(0.5)
+    }
+  }, [animation]);
+
+  useEffect(() => {
+    Object.values(materials).forEach((material) => {
+      material.wireframe = wireframe
+    })
+  }, [wireframe]);
 
   return (
     <group {...props} ref={group} dispose={null}>
